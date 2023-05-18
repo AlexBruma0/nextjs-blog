239f734848dd69875231d9575056dcfbf38c1dde
AlexBruma0 <105242355+AlexBruma0@users.noreply.github.com>
2023-05-18 13:15:15 -0700

    title

--- pages/index.js
+++ pages/index.js
@@ -26,8 +26,8 @@ export default function Home({ allPostsData }) {
 
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (Check out my calendar web-app{' '}
+          <a href="https://calendar22-e0034.firebaseapp.com/">here</a>.)
         </p>
       </section>
 
